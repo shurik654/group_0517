61a519cc5b0eabb47ecf439918af092b1f97cc81
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-02 18:23:59 +0300

    Create Main.java

---
+++ Task_2.6/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int a=1; a<10; a++){
+        for (int b=1; b<10; b++) {
+            System.out.print(a*b +" ");
+        }
+            System.out.println(" ");
+        }
+    }
+}
