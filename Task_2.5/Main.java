d380de8502fa781244e4f5dcccf8105febb665cf
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-02 18:09:40 +0300

    Create Main.java

---
+++ Task_2.5/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "Мама";
+        String b = "Мыла";
+        String i = "Раму";
+        System.out.println(a+b+i);
+        System.out.println(a+i+b);
+        System.out.println(b+a+i);
+        System.out.println(b+i+a);
+        System.out.println(i+b+a);
+        System.out.println(i+a+b);
+    }
+}
