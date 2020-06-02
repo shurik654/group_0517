5a4aa4801f275a0df12dd83894ef39d03532a651
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-02 19:30:06 +0300

    Create Main.java

---
+++ Task_2.7/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+        int a = 5;
+        int b = 4;
+        int c = 6;
+        return a+b+c;
+        }
+        }
