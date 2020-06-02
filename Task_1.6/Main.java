76cc0af0d723a80835349f13829498c8b3030e15
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-02 14:07:40 +0300

    Create Main.java

---
+++ Task_1.6/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+                int a,b;
+               a = scanner.nextInt();
+               b = a/100*15;
+        System.out.println(a+b);
+    }
+}
