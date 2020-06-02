7e78ef1554870f094504484cb7bf0facb1f91927
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-02 17:26:17 +0300

    Create Main.java

---
+++ Task_2.3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 10;
+        while (a != 10) {
+            b *= a;
+            a++; }
+        System.out.println(b);
