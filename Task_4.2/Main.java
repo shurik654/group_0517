059f71ead9b07c491bf5cce2187d2de2c52264bb
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-04 18:49:31 +0300

    Create Main.java

---
+++ Task_4.2/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Mouse tuffiMouse = new Mouse("Tuffi",8,3);
+        Cat tomCat = new Cat("Tom", 25, 20);
+        Cat butchCat = new Cat("Butch",28,20);
+        Dog spikeDog = new Dog("Spike",58,15);
+        Dog taykDog = new Dog("Tayk",20,8);
+    }
+}
+class Mouse {
+    String name;
+    int height;
+    int tail;
+
+    public Mouse(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Cat {
+    String name;
+    int height;
+    int tail;
+
+    public Cat(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Dog {
+    String name;
+    int height;
+    int tail;
+
+    public Dog(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
