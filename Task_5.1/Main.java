00c5b14fd95016ef545e36ba1f47784c46e95281
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-06 19:53:34 +0300

    Create Main.java

---
+++ Task_5.1/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Модернизация ПО
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<String> list = new ArrayList<>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) {
+                break;
+            }
+
+            list.add(family);
+        }
+
+        String city = reader.readLine();
+        for (int i = 0; i < list.size(); i++) {
+            if (city.equals(list.get(i))) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
