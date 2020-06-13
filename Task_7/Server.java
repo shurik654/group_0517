3f63325bebcbc28d02904b52707048eb109ee076
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-13 21:49:19 +0300

    Update Server.java

--- Task_7/Server.java
+++ Task_7/Server.java
@@ -11,19 +11,19 @@ public class Server {
         Socket socket = null;
         try{
             ServerSocket serverSocket = new ServerSocket(8189);
-            System.out.println("Сервер запущен");
+            System.out.println("Server is running");
             while(true) {
                 socket = serverSocket.accept();
                 clients.add(socket);
                 DataInputStream in = new DataInputStream(socket.getInputStream());
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                String username = in.readUTF();
-                System.out.println("Добавлен(а) "+username);
-                out.writeUTF("Добавлен(а) "+username);
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
+                            String username = in.readUTF();
+                            System.out.println("Joined "+username);
+                            out.writeUTF("Joined "+username);
                             while (true) {
                                 String str =in.readUTF();
                                 broadcastMsg(str);
