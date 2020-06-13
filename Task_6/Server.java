57496c60719194dff41bd8d5d5083495ae769ae3
shurik654 <33161710+shurik654@users.noreply.github.com>
2020-06-13 20:51:17 +0300

    Create Server.java

---
+++ Task_6/Server.java
@@ -0,0 +1,50 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+    static ArrayList<Socket> clients= new ArrayList<>();
+    public static void main(String[] args) {
+        Socket socket = null;
+        try{
+            ServerSocket serverSocket = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+            while(true) {
+                socket = serverSocket.accept();
+                clients.add(socket);
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                String username = in.readUTF();
+                System.out.println("Добавлен(а) "+username);
+                out.writeUTF("Добавлен(а) "+username);
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (true) {
+                                String str =in.readUTF();
+                                broadcastMsg(str);
+                                System.out.println(str);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static void broadcastMsg(String str) throws IOException {
+        DataOutputStream out;
+        for (Socket socket: clients){
+            out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(str);
+        }
+    }
+}
